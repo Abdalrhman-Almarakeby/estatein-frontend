7a3d062970c03b087520bd33eabebee5a04c475e
Abdalrhman Almarakeby <aalmarakeby@gmail.com>
2024-02-27 07:37:28 -0800

    Add design colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
+/* eslint-env node */
 module.exports = {
   darkMode: ["class"],
-  content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-  ],
+  content: ["./src/**/*.{ts,tsx}"],
   prefix: "",
   theme: {
     container: {
@@ -31,7 +27,32 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      colors: {
+        // purple shades
+        "purple-60": "#FF7A00",
+        "purple-65": "#1A64FF",
+        "purple-70": "#FF9533",
+        "purple-75": "#FFCA99",
+        "purple-90": "#FFE4CC",
+        "purple-95": "#FFF1E5",
+        "purple-97": "#FFF7F0",
+        "purple-99": "#FFFCFA",
+        // white shades
+        "white-90": "#E4E4E7",
+        "white-95": "#F1F1F3",
+        "white-97": "#F7F7F8",
+        "white-99": "#FCFCFD",
+        // gray shades
+        "gray-08": "#141414",
+        "gray-10": "#1A1A1A",
+        "gray-15": "#262626",
+        "gray-20": "#333333",
+        "gray-30": "#4D4D4D",
+        "gray-40": "#666666",
+        "gray-50": "#808080",
+        "gray-60": "#999999",
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
