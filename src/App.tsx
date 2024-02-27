09438530393c12447e772a03fbb0e0c21f6f7e86
Abdalrhman Almarakeby <aalmarakeby@gmail.com>
2024-02-26 16:05:01 -0800

    Create the basic routes and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Properties from "./pages/Properties";
+import Services from "./pages/Services";
+import Contact from "./pages/Contact";
 import Error from "./pages/Error";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import "./CSS/global.css";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="*" element={<Error />} />
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/properties" element={<Properties />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
