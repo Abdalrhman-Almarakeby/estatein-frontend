0d0c653527ddfd15711849077dd984ca63c40c59
Abdalrhman Almarakeby <aalmarakeby@gmail.com>
2024-03-14 15:14:26 +0200

    Fix Home Carousels.

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,3 +1,8 @@
+import { useFetch } from "@/lib/hooks/useFetch";
+import { type Property } from "@/types/property";
+import { testimonials } from "@/constant/testimonials";
+import { faqs } from "@/constant/faqs";
+import Loading from "@/components/ui/Loading";
 import PropertyCard from "@/components/ui/PropertyCard";
 import TestimonialCard from "@/components/ui/TestimonialCard";
 import FAQCard from "@/components/ui/FAQCard";
@@ -6,24 +11,37 @@ import { HomeCarouselSection, CarouselItem } from "./HomeCarouselSection";
 import CTASection from "./CTASection";
 
 export default function Home() {
+  const {
+    data: propertiesData,
+    isPending,
+    error,
+  } = useFetch<Property[]>("http://localhost:8080/properties?limit=60");
+
   return (
     <main className="flex-grow bg-gray-08">
+      {isPending && <Loading />}
+      {error && <p>{error}</p>}
       <div className="container space-y-20 lg:space-y-30">
         <HeroSection />
 
-        <HomeCarouselSection
-          id="featured-properties"
-          title="Featured Properties"
-          paragraph="Explore our handpicked selection of featured properties. Each listing offers a glimpse into exceptional homes and investments available through Estatein."
-          link="/properties"
-          sectionName="Properties"
-        >
-          {Array.from({ length: 60 }).map((_, index) => (
-            <CarouselItem key={index} className="md:basis-1/2 md:pl-5 xl:basis-1/3">
-              <PropertyCard />
-            </CarouselItem>
-          ))}
-        </HomeCarouselSection>
+        {propertiesData && (
+          <HomeCarouselSection
+            id="featured-properties"
+            title="Featured Properties"
+            paragraph="Explore our handpicked selection of featured properties. Each listing offers a glimpse into exceptional homes and investments available through Estatein."
+            link="/properties"
+            sectionName="Properties"
+          >
+            {propertiesData.map((propertyData) => {
+              return (
+                <CarouselItem key={propertyData._id} className="md:basis-1/2 md:pl-5 xl:basis-1/3">
+                  <PropertyCard {...propertyData} />
+                </CarouselItem>
+              );
+            })}
+          </HomeCarouselSection>
+        )}
+
         <HomeCarouselSection
           id="testimonials"
           title="What Our Clients Say"
@@ -31,12 +49,13 @@ export default function Home() {
           link="/"
           sectionName="Testimonials"
         >
-          {Array.from({ length: 10 }).map((_, index) => (
-            <CarouselItem key={index} className="md:basis-1/2 md:pl-5 xl:basis-1/3">
-              <TestimonialCard />
+          {testimonials.map((testimonial) => (
+            <CarouselItem key={testimonial.name} className="md:basis-1/2 md:pl-5 xl:basis-1/3">
+              <TestimonialCard {...testimonial} />
             </CarouselItem>
           ))}
         </HomeCarouselSection>
+
         <HomeCarouselSection
           id="featured-properties"
           title="Frequently Asked Questions"
@@ -44,9 +63,9 @@ export default function Home() {
           link="/"
           sectionName="FAQs"
         >
-          {Array.from({ length: 10 }).map((_, index) => (
-            <CarouselItem key={index} className="md:basis-1/2 md:pl-5 xl:basis-1/3">
-              <FAQCard />
+          {faqs.map((faq) => (
+            <CarouselItem key={faq.question} className="md:basis-1/2 md:pl-5 xl:basis-1/3">
+              <FAQCard {...faq} />
             </CarouselItem>
           ))}
         </HomeCarouselSection>
