e3fdc20188d8872d2cde068c20717d3c37216bf7
Abdalrhman Almarakeby <aalmarakeby@gmail.com>
2024-03-16 16:34:31 +0200

    Fix markup.

--- frontend/src/pages/Home/Achievement.tsx
+++ frontend/src/pages/Home/Achievement.tsx
@@ -8,14 +8,16 @@ type AchievementProps = {
 
 export default function Achievement({ achievement, label, className }: AchievementProps) {
   return (
-    <div
+    <h2
       className={cn(
         "last:col-span-2 space-y-0.5 border border-gray-15 bg-gray-10 p-4 sm:!col-span-1",
         className
       )}
     >
-      <h4 className="block text-2xl font-bold xl:text-3xl 3xl:text-4xl">{achievement}</h4>
-      <p className="block text-sm font-medium text-gray-60 2xl:text-base 3xl:text-lg">{label}</p>
-    </div>
+      <span className="block text-2xl font-bold xl:text-3xl 3xl:text-4xl">{achievement}</span>
+      <span className="block text-sm font-medium text-gray-60 2xl:text-base 3xl:text-lg">
+        {label}
+      </span>
+    </h2>
   );
 }
