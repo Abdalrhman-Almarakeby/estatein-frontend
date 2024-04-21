415de075a719f6cd87b8e68982c8af62fb227a29
Abdalrhman Almarakeby <aalmarakeby@gmail.com>
2024-04-21 20:50:15 +0200

    Add error handling.

--- frontend/src/pages/Home/HomeCarouselSection.tsx
+++ frontend/src/pages/Home/HomeCarouselSection.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import SectionTitle from "@/components/ui/SectionTitle";
 import {
   type CarouselApi,
@@ -8,7 +9,7 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/Carousel";
-import { Link } from "react-router-dom";
+import ErrorSVG from "@/assets/icons/error.svg?react";
 
 type HomeCarouselSectionProps = {
   id: string;
@@ -17,6 +18,8 @@ type HomeCarouselSectionProps = {
   paragraph: string;
   link: string;
   children: React.ReactNode[];
+  isError?: boolean;
+  retry?: () => void;
 };
 
 function HomeCarouselSection({
@@ -26,6 +29,8 @@ function HomeCarouselSection({
   paragraph,
   link,
   children,
+  isError,
+  retry,
 }: HomeCarouselSectionProps) {
   const [api, setApi] = useState<CarouselApi>();
   const [current, setCurrent] = useState(0);
@@ -54,28 +59,39 @@ function HomeCarouselSection({
         </Link>
       </div>
 
-      <Carousel
-        setApi={setApi}
-        opts={{
-          align: "start",
-        }}
-        className="flex flex-col gap-7.5"
-      >
-        <CarouselContent className="md:-ml-5">{children}</CarouselContent>
-        <div className="flex flex-wrap items-center justify-between gap-2.5 border-t border-t-gray-15 pt-4 text-sm min-450:flex-nowrap min-450:justify-normal md:justify-end md:gap-5">
-          <Link
-            to={link}
-            className="order-last mr-auto basis-full rounded-lg border border-gray-15 px-5 py-3.5 text-center min-450:order-none min-450:basis-auto lg:hidden"
-          >
-            View All {sectionName}
-          </Link>
-          <CarouselPrevious className="static translate-x-0 translate-y-0 lg:order-2" />
-          <p className="px-5 text-gray-60 md:hidden">
-            <span className="text-white">{current}</span> of {count}
-          </p>
-          <CarouselNext className="static translate-x-0 translate-y-0 lg:order-3" />
+      {!isError ? (
+        <Carousel
+          setApi={setApi}
+          opts={{
+            align: "start",
+          }}
+          className="flex flex-col gap-7.5"
+        >
+          <CarouselContent className="md:-ml-5">{children}</CarouselContent>
+          <div className="flex flex-wrap items-center justify-between gap-2.5 border-t border-t-gray-15 pt-4 text-sm min-450:flex-nowrap min-450:justify-normal md:justify-end md:gap-5">
+            <Link
+              to={link}
+              className="order-last mr-auto basis-full rounded-lg border border-gray-15 px-5 py-3.5 text-center min-450:order-none min-450:basis-auto lg:hidden"
+            >
+              View All {sectionName}
+            </Link>
+            <CarouselPrevious className="static translate-x-0 translate-y-0 lg:order-2" />
+            <p className="px-5 text-gray-60 md:hidden">
+              <span className="text-white">{current}</span> of {count}
+            </p>
+            <CarouselNext className="static translate-x-0 translate-y-0 lg:order-3" />
+          </div>
+        </Carousel>
+      ) : (
+        <div className="flex h-[485px] flex-col items-center justify-center gap-4 text-balance rounded-md border border-gray-15 text-center text-lg sm:h-[580px] md:h-[454px] lg:h-[500px] xl:h-[480px] 2xl:h-[510px]">
+          <ErrorSVG />
+          There was a problem connecting to the server. Please check your internet connection and
+          try again.
+          <button onClick={retry} className="rounded-lg bg-purple-60 px-5 py-2 text-base">
+            Try agin
+          </button>
         </div>
-      </Carousel>
+      )}
     </section>
   );
 }
