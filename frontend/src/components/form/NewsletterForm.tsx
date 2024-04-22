96477415637144bffb9a43993e715c8574c6d4c7
Abdalrhman Almarakeby <aalmarakeby@gmail.com>
2024-04-22 13:43:41 +0200

    Use react hook form and react query to build  NewsletterForm.

--- frontend/src/components/form/NewsletterForm.tsx
+++ frontend/src/components/form/NewsletterForm.tsx
@@ -1,26 +1,57 @@
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
 import { Input } from "./Input.tsx";
 import MailPlusIconSVG from "@/assets/icons/email-plus.svg?react";
 import MailSendingIconSVG from "@/assets/icons/mail-sending.svg?react";
+import { EmailSchema, emailSchema } from "@/types/emailSchema.ts";
 
 export function NewsletterForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<EmailSchema>({
+    resolver: zodResolver(emailSchema),
+  });
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: async (email: string) => {
+      const { data } = await axios.post("http://localhost:1111/newsletter", {
+        email,
+      });
+      console.log(data);
+    },
+  });
+
+  function onSubmit(data: EmailSchema) {
+    mutate(data.email);
+    reset();
+  }
+
   return (
-    <form className="space-y-3 lg:w-[300px]">
+    <form onSubmit={handleSubmit(onSubmit)} className="space-y-3 lg:w-[300px]">
       <p className="px-1 text-lg">Subscribe to our newsletter!</p>
       <div className="relative">
         <MailPlusIconSVG className="absolute left-5 top-1/2 -translate-y-1/2" aria-hidden="true" />
         <Input
-          type="email"
-          name="email"
-          required
-          placeholder="Enter Email"
+          {...register("email")}
+          placeholder="Email"
           autoComplete="email"
           className="w-full px-12 py-3.5"
         />
-        <button aria-label="Subscribe" className="absolute right-5 top-1/2 -translate-y-1/2">
+        <button
+          disabled={isPending}
+          aria-label="Subscribe"
+          className="absolute right-5 top-1/2 -translate-y-1/2"
+        >
           <MailSendingIconSVG aria-hidden="true" />
           <span className="sr-only">Subscribe To Our News Letter</span>
         </button>
       </div>
+      {errors.email && <p className=" text-sm text-red-500">{errors.email.message}</p>}
     </form>
   );
 }
