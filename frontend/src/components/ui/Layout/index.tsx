ab5392b82eefb587d23b0abb108ff4c105036395
Abdalrhman Almarakeby <aalmarakeby@gmail.com>
2024-04-22 15:25:42 +0200

    Update Layout component.

--- frontend/src/components/ui/Layout/index.tsx
+++ frontend/src/components/ui/Layout/index.tsx
@@ -1,19 +1,8 @@
-import { useEffect } from "react";
-import { useLocation, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
 
 export function Layout() {
-  const { pathname } = useLocation();
-
-  useEffect(() => {
-    document.documentElement.scrollTo({
-      top: 0,
-      left: 0,
-      behavior: "instant",
-    });
-  }, [pathname]);
-
   return (
     <div className="flex min-h-svh flex-col pt-[82px] md:p-0">
       <Header />
