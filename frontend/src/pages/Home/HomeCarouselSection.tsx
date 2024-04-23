0383b9f7c0363fc1f6fb46a0117199a779d6be1e
Abdalrhman Almarakeby <aalmarakeby@gmail.com>
2024-04-23 23:22:34 +0200

    Remove the CarouselAPI and memoize the component.

--- frontend/src/pages/Home/HomeCarouselSection.tsx
+++ frontend/src/pages/Home/HomeCarouselSection.tsx
@@ -1,8 +1,7 @@
-import { useEffect, useState } from "react";
+import { memo } from "react";
 import { Link } from "react-router-dom";
 import { SectionTitle } from "@/components/ui/SectionTitle";
 import {
-  type CarouselApi,
   Carousel,
   CarouselContent,
   CarouselItem,
@@ -22,78 +21,61 @@ type HomeCarouselSectionProps = {
   retry?: () => void;
 };
 
-function HomeCarouselSection({
-  id,
-  sectionName,
-  title,
-  paragraph,
-  link,
-  children,
-  isError,
-  retry,
-}: HomeCarouselSectionProps) {
-  const [api, setApi] = useState<CarouselApi>();
-  const [current, setCurrent] = useState(0);
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    if (!api) return;
-
-    setCount(api.scrollSnapList().length);
-    setCurrent(api.selectedScrollSnap() + 1);
-
-    api.on("select", () => {
-      setCurrent(api.selectedScrollSnap() + 1);
-    });
-  }, [api]);
-
-  return (
-    <section id={id} className="space-y-10 md:space-y-12 lg:space-y-15">
-      <div className="flex justify-between">
-        <SectionTitle title={title} paragraph={paragraph} />
-        <Link
-          to={link}
-          className="hidden self-end rounded-lg border border-gray-15 px-5 py-3.5 text-center text-sm lg:block"
-        >
-          View All {sectionName}
-        </Link>
-      </div>
-
-      {!isError ? (
-        <Carousel
-          setApi={setApi}
-          opts={{
-            align: "start",
-          }}
-          className="flex flex-col gap-7.5"
-        >
-          <CarouselContent className="md:-ml-5">{children}</CarouselContent>
-          <div className="flex flex-wrap items-center justify-between gap-2.5 border-t border-t-gray-15 pt-4 text-sm min-450:flex-nowrap min-450:justify-normal md:justify-end md:gap-5">
-            <Link
-              to={link}
-              className="order-last mr-auto basis-full rounded-lg border border-gray-15 px-5 py-3.5 text-center min-450:order-none min-450:basis-auto lg:hidden"
-            >
-              View All {sectionName}
-            </Link>
-            <CarouselPrevious className="static translate-x-0 translate-y-0 lg:order-2" />
-            <p className="px-5 text-gray-60 md:hidden">
-              <span className="text-white">{current}</span> of {count}
-            </p>
-            <CarouselNext className="static translate-x-0 translate-y-0 lg:order-3" />
-          </div>
-        </Carousel>
-      ) : (
-        <div className="flex h-[485px] flex-col items-center justify-center gap-4 text-balance rounded-md border border-gray-15 text-center text-lg sm:h-[580px] md:h-[454px] lg:h-[500px] xl:h-[480px] 2xl:h-[510px]">
-          <ErrorSVG />
-          There was a problem connecting to the server. Please check your internet connection and
-          try again.
-          <button onClick={retry} className="rounded-lg bg-purple-60 px-5 py-2 text-base">
-            Try agin
-          </button>
+const HomeCarouselSection = memo(
+  ({
+    id,
+    sectionName,
+    title,
+    paragraph,
+    link,
+    children,
+    isError,
+    retry,
+  }: HomeCarouselSectionProps) => {
+    return (
+      <section id={id} className="space-y-10 md:space-y-12 lg:space-y-15">
+        <div className="flex justify-between">
+          <SectionTitle title={title} paragraph={paragraph} />
+          <Link
+            to={link}
+            className="hidden self-end rounded-lg border border-gray-15 px-5 py-3.5 text-center text-sm lg:block"
+          >
+            View All {sectionName}
+          </Link>
         </div>
-      )}
-    </section>
-  );
-}
+
+        {!isError ? (
+          <Carousel
+            opts={{
+              align: "start",
+            }}
+            className="flex flex-col gap-7.5"
+          >
+            <CarouselContent className="md:-ml-5">{children}</CarouselContent>
+            <div className="flex flex-wrap items-center justify-between gap-2.5 border-t border-t-gray-15 pt-4 text-sm min-450:flex-nowrap min-450:justify-normal md:justify-end md:gap-5">
+              <Link
+                to={link}
+                className="order-last mr-auto basis-full rounded-lg border border-gray-15 px-5 py-3.5 text-center min-450:order-none min-450:basis-auto lg:hidden"
+              >
+                View All {sectionName}
+              </Link>
+              <CarouselPrevious className="static translate-x-0 translate-y-0 lg:order-2" />
+              <CarouselNext className="static translate-x-0 translate-y-0 lg:order-3" />
+            </div>
+          </Carousel>
+        ) : (
+          <div className="flex h-[485px] flex-col items-center justify-center gap-4 text-balance rounded-md border border-gray-15 text-center text-lg sm:h-[580px] md:h-[454px] lg:h-[500px] xl:h-[480px] 2xl:h-[510px]">
+            <ErrorSVG />
+            There was a problem connecting to the server. Please check your internet connection and
+            try again.
+            <button onClick={retry} className="rounded-lg bg-purple-60 px-5 py-2 text-base">
+              Try agin
+            </button>
+          </div>
+        )}
+      </section>
+    );
+  }
+);
 
 export { HomeCarouselSection, CarouselItem };
