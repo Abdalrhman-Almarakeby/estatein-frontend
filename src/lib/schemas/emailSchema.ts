16f9ed2cc8a041436651e9acf8d7af06b163a5c9
Abdalrhman Almarakeby <aalmarakeby@gmail.com>
2024-05-05 19:37:01 +0300

    Update newsletter form error messages.

--- src/lib/schemas/emailSchema.ts
+++ src/lib/schemas/emailSchema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const emailZodSchema = z.object({
-  email: z.string().email(),
+  email: z.string().min(1, "PLease enter you email").email("PLease enter a valid email"),
 });
 
 export type Email = z.infer<typeof emailZodSchema>;
