5471add73e3490168323cfe2645d397e16bc783c
Abdalrhman Almarakeby <aalmarakeby@gmail.com>
2024-05-05 19:25:47 +0300

    Update FAQCard styles.

--- src/components/ui/FAQCard.tsx
+++ src/components/ui/FAQCard.tsx
@@ -8,11 +8,11 @@ type FAQCardProps = {
 export function FAQCard({ question, answer }: FAQCardProps) {
   return (
     <div className="flex flex-col justify-between gap-5 rounded-xl border p-7.5 lg:gap-6 lg:p-10">
-      <p className="text-lg lg:text-xl">{question}</p>
+      <p className="text-lg lg:text-xl 3xl:text-2xl">{question}</p>
       <p className="text-primary">{answer}</p>
       <Link
         to="/about#how-it-is-work"
-        className="btn-tertiary btn-sm 3xl:btn-md 3xl:text-lg text-center"
+        className="btn-tertiary btn-sm 3xl:btn-md text-center 3xl:text-lg"
       >
         Read More
       </Link>
