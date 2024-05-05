188ced8d734e65f0877c60d2e3163100de69fdf1
Abdalrhman Almarakeby <aalmarakeby@gmail.com>
2024-05-05 19:18:15 +0300

    Update TestimonialCard styles.

--- src/components/ui/TestimonialCard.tsx
+++ src/components/ui/TestimonialCard.tsx
@@ -12,20 +12,20 @@ export function TestimonialCard({
   avatar,
 }: TestimonialCardProps) {
   return (
-    <div className="flex flex-col justify-between gap-6 rounded-xl border  p-7.5 lg:gap-7.5 lg:p-10">
+    <div className="flex flex-col justify-between gap-6 rounded-xl border p-7.5 lg:gap-7.5 lg:p-10">
       <div className="flex gap-2">
         {Array.from({ length: rating }).map((_, index) => (
           <span
             key={index}
-            className="grid size-7.5 place-items-center rounded-full border  bg-gray-10 lg:size-9"
+            className="grid size-7.5 place-items-center rounded-full border bg-gray-10 lg:size-9"
           >
             <StarSVG className="lg:size-5" />
           </span>
         ))}
       </div>
       <div className="space-y-2 lg:space-y-3">
-        <p className="text-lg lg:text-xl">{title}</p>
-        <p className="test-secondary">{body}</p>
+        <p className="text-lg lg:text-xl 3xl:text-2xl">{title}</p>
+        <p className="text-primary">{body}</p>
       </div>
       <div className="flex gap-2.5">
         <img
