212ea3cda8af16071ef1e37d658e668d70bb0976
Abdalrhman Almarakeby <aalmarakeby@gmail.com>
2024-05-05 10:14:21 +0300

    Mutate Inquiry in the contact form.

--- src/pages/Contact/useContactForm.ts
+++ src/pages/Contact/useContactForm.ts
@@ -1,14 +1,22 @@
-import { type BaseSyntheticEvent } from "react";
+import { useState, type BaseSyntheticEvent } from "react";
 import { useForm, type UseFormRegister, type FieldErrors, Control } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import { toast } from "react-hot-toast";
 import { contactSchema, type ContactSchema } from "@/lib/schemas/contactSchema";
+import { omit } from "@/lib/utils/object";
+
+type InquirySchema = Omit<ContactSchema, "agreeOnTerms">;
 
 export function useContactForm(): {
   register: UseFormRegister<ContactSchema>;
   onSubmit: (e?: BaseSyntheticEvent<object> | undefined) => Promise<void>;
   errors: FieldErrors<ContactSchema>;
   control: Control<ContactSchema>;
+  isPending: boolean;
 } {
+  const [toastId, setToastId] = useState<string | null>(null);
   const {
     register,
     handleSubmit,
@@ -19,8 +27,24 @@ export function useContactForm(): {
     resolver: zodResolver(contactSchema),
   });
 
+  const { mutate, isPending } = useMutation({
+    mutationFn: async (inquiryData: InquirySchema) =>
+      axios.post("http://localhost:1111/inquiries", {
+        inquiryData,
+      }),
+    onMutate: () => setToastId(toast.loading("Sending Your Message...")),
+    onSuccess: () =>
+      toast.success("Your message is sent successfully!", {
+        id: toastId || undefined,
+      }),
+    onError: () =>
+      toast.error("something went wrong, please try again later!", {
+        id: toastId || undefined,
+      }),
+  });
+
   function onSubmit(data: ContactSchema) {
-    data;
+    mutate(omit(data, "agreeOnTerms"));
     reset();
   }
 
@@ -29,5 +53,6 @@ export function useContactForm(): {
     onSubmit: handleSubmit(onSubmit),
     errors,
     control,
+    isPending,
   };
 }
