f740a25c48a1ba8acf40f952788be35d1996c3e3
Abdalrhman Almarakeby <aalmarakeby@gmail.com>
2024-05-11 13:25:26 +0300

    Build Galley component.

---
+++ src/constant/galleryImages.ts
@@ -0,0 +1,33 @@
+import img1 from "@/assets/imgs/1.webp";
+import img2 from "@/assets/imgs/2.webp";
+import img3 from "@/assets/imgs/3.webp";
+import img4 from "@/assets/imgs/4.webp";
+import img5 from "@/assets/imgs/5.webp";
+import img6 from "@/assets/imgs/6.webp";
+
+export const GALLERY_IMAGES = [
+  {
+    src: img1,
+    alt: "Some place holder alt text 1",
+  },
+  {
+    src: img2,
+    alt: "Some place holder alt text 2",
+  },
+  {
+    src: img3,
+    alt: "Some place holder alt text 3",
+  },
+  {
+    src: img4,
+    alt: "Some place holder alt text 4",
+  },
+  {
+    src: img5,
+    alt: "Some place holder alt text 5",
+  },
+  {
+    src: img6,
+    alt: "Some place holder alt text 6",
+  },
+] as const;
---
+++ src/pages/Contact/Gallery.tsx
@@ -0,0 +1,25 @@
+import { SectionTitle } from "@/components/ui/SectionTitle";
+import { GALLERY_IMAGES } from "@/constant/galleryImages";
+import WavesSVG from "@/assets/abstractDesigns/waves.svg?react";
+
+export function Gallery() {
+  return (
+    <div className="relative grid grid-cols-4 gap-2.5 rounded-xl border p-6 lg:gap-5 lg:p-15 3xl:p-20">
+      <WavesSVG className="absolute left-0 top-0 h-full w-full" />
+      {GALLERY_IMAGES.map(({ src, alt }) => (
+        <img
+          key={src}
+          src={src}
+          alt={alt}
+          fetchPriority="low"
+          className="z-10 aspect-[2/1] w-full rounded-[0.625rem] object-cover last-of-type:order-last last-of-type:col-span-4 last-of-type:mt-2.5 last-of-type:object-cover last-of-type:object-top last-of-type:md:col-span-2 last-of-type:md:mt-0 [&:nth-of-type(-n+3)]:col-span-2 [&:nth-of-type(4)]:aspect-square [&:nth-of-type(5)]:aspect-square"
+        />
+      ))}
+      <SectionTitle
+        title="Explore Estatein's World"
+        paragraph="Step inside the world of Estatein, where professionalism meets warmth, and expertise meets passion. Our gallery offers a glimpse into our team and workspaces, inviting you to get to know us better."
+        className="z-10 col-span-4 mt-11 self-center md:col-span-2 md:my-7.5"
+      />
+    </div>
+  );
+}
