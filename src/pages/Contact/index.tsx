af06ef6285a7ccd4d46261f575fe23c59f0fdb1c
Abdalrhman Almarakeby <aalmarakeby@gmail.com>
2024-05-11 13:25:40 +0300

    Finish Contact page.

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -1,7 +1,9 @@
 import { HeaderSection } from "@/components/ui/HeaderSection";
 import { LinksSection } from "@/components/ui/LinksSection";
+import { CTA } from "@/components/ui/CTA";
 import { ContactFormSection } from "./ContactFormSection";
 import { OfficeLocations } from "./OfficeLocations";
+import { Gallery } from "./Gallery";
 import PhoneIconsSVG from "@/assets/icons/phone.svg?react";
 import MailIconSVG from "@/assets/icons/mail.svg?react";
 import LocationIconSVG from "@/assets/icons/location.svg?react";
@@ -38,16 +40,18 @@ const LINKS_DATA = [
 
 export function Contact() {
   return (
-    <div className="container min-h-[10000px] flex-grow space-y-20 lg:space-y-30">
+    <div className="container flex-grow space-y-20 lg:space-y-30">
       <div>
         <HeaderSection
           title="Get in Touch with Estatein"
-          p="Welcome to Estatein's Contact Us page. We're here to assist you with any inquiries, requests, or feedback you may have. Whether you're looking to buy or sell a property, explore investment opportunities, or simply want to connect, we're just a message away. Reach out to us, and let's start a conversation."
+          paragraph="Welcome to Estatein's Contact Us page. We're here to assist you with any inquiries, requests, or feedback you may have. Whether you're looking to buy or sell a property, explore investment opportunities, or simply want to connect, we're just a message away. Reach out to us, and let's start a conversation."
         />
         <LinksSection linksData={LINKS_DATA} />
       </div>
       <ContactFormSection />
       <OfficeLocations />
+      <Gallery />
+      <CTA />
     </div>
   );
 }
