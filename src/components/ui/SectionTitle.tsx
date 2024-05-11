b374729dbd852d3245ae7b0fea8bcadf83e3463d
Abdalrhman Almarakeby <aalmarakeby@gmail.com>
2024-05-11 13:25:01 +0300

    Update SectionTitle component.

--- src/components/ui/SectionTitle.tsx
+++ src/components/ui/SectionTitle.tsx
@@ -1,13 +1,15 @@
 import StarsSVG from "@/assets/abstractDesigns/stars.svg?react";
+import { cn } from "@/lib/utils/cn";
 
 type SectionTitleProps = {
   title: string;
   paragraph: string;
+  className?: string;
 };
 
-export function SectionTitle({ title, paragraph }: SectionTitleProps) {
+export function SectionTitle({ title, paragraph, className }: SectionTitleProps) {
   return (
-    <div className="relative space-y-1.5 lg:space-y-2.5">
+    <div className={cn("relative space-y-1.5 lg:space-y-2.5", className)}>
       <StarsSVG className="absolute -left-2 -translate-y-full lg:h-6" />
       <h3 className="h-primary">{title}</h3>
       <p className="text-primary">{paragraph}</p>
