627593b918af11a56bac894b6dbb3fe0a888e8b5
Abdalrhman Almarakeby <aalmarakeby@gmail.com>
2024-05-11 13:53:59 +0300

    Update HeaderSection component.

--- src/components/ui/HeaderSection.tsx
+++ src/components/ui/HeaderSection.tsx
@@ -5,11 +5,11 @@ type HeaderSectionProps = {
 
 export function HeaderSection({ title, paragraph }: HeaderSectionProps) {
   return (
-    <div className="mx-break-out px-break-out bg-gradient-to-l from-transparent from-55% to-gray-15">
+    <section className="mx-break-out px-break-out bg-gradient-to-l from-transparent from-55% to-gray-15">
       <div className="container space-y-2.5 py-12.5 lg:py-24 3xl:space-y-3.5 3xl:py-36">
         <h1 className="h-primary">{title}</h1>
         <p className="text-primary">{paragraph}</p>
       </div>
-    </div>
+    </section>
   );
 }
