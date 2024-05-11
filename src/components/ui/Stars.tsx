2720b8032756aa0dd482a93d7332caaba3d904b4
Abdalrhman Almarakeby <aalmarakeby@gmail.com>
2024-05-11 18:52:01 +0300

    Improve Stars component accessability.

--- src/components/ui/Stars.tsx
+++ src/components/ui/Stars.tsx
@@ -8,11 +8,12 @@ type StarsProps = {
 
 export function Stars({ num, className }: StarsProps) {
   return (
-    <div className={cn("flex gap-2", className)}>
+    <div className={cn("flex gap-2", className)} aria-label={`${num} stars`}>
       {Array.from({ length: num }).map((_, index) => (
         <span
           key={index}
           className="grid size-7.5 place-items-center rounded-full border bg-gray-10 lg:size-9"
+          aria-hidden="true"
         >
           <StarSVG className="lg:size-5" />
         </span>
