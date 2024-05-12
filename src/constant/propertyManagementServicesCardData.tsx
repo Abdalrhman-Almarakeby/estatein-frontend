12af53d91a4ca5c15129bbe331d13923ad1cf965
Abdalrhman Almarakeby <aalmarakeby@gmail.com>
2024-05-12 11:04:27 +0300

    Move Services page CTA card data to it is own file.

---
+++ src/constant/propertyManagementServicesCardData.tsx
@@ -0,0 +1,6 @@
+export const PROPERTY_MANAGEMENT_SERVICES_CTA_CARD_DATA = {
+  title: "Experience Effortless Property Management",
+  paragraph:
+    "Ready to experience hassle-free property management? Explore our Property Management Service categories and let us handle the complexities while you enjoy the benefits of property ownership.",
+  link: "/contact#contact",
+} as const;
--- src/pages/Services/index.tsx
+++ src/pages/Services/index.tsx
@@ -3,6 +3,8 @@ import { LinksSection } from "@/components/ui/LinksSection";
 import { SERVICES_PAGE_LINKS_DATA } from "@/constant/pageLinksData/services";
 import { PROPERTIES_SERVICES } from "@/constant/propertiesServices";
 import { PROPERTY_MANAGEMENT_SERVICES } from "@/constant/propertyManagementServices";
+import { PROPERTIES_SERVICES_CTA_CARD_DATA } from "@/constant/propertiesServicesCardData";
+import { PROPERTY_MANAGEMENT_SERVICES_CTA_CARD_DATA } from "@/constant/propertyManagementServicesCardData";
 import { ServicesSection } from "./ServicesSection";
 
 export function Services() {
@@ -18,23 +20,13 @@ export function Services() {
       <ServicesSection
         title="Unlock Property Value"
         paragraph="Selling your property should be a rewarding experience, and at Estatein, we make sure it is."
-        CTACardData={{
-          title: "Unlock the Value of Your Property Today",
-          paragraph:
-            "Ready to unlock the true value of your property? Explore our Property Selling Service categories and let us help you achieve the best deal possible for your valuable asset.",
-          link: "/contact#contact",
-        }}
+        CTACardData={PROPERTIES_SERVICES_CTA_CARD_DATA}
         servicesData={PROPERTIES_SERVICES}
       />
       <ServicesSection
         title="Effortless Property Management"
         paragraph="Owning a property should be a pleasure, not a hassle. Estatein's Property Management Service takes the stress out of property ownership."
-        CTACardData={{
-          title: "Experience Effortless Property Management",
-          paragraph:
-            "Ready to experience hassle-free property management? Explore our Property Management Service categories and let us handle the complexities while you enjoy the benefits of property ownership.",
-          link: "/contact#contact",
-        }}
+        CTACardData={PROPERTY_MANAGEMENT_SERVICES_CTA_CARD_DATA}
         servicesData={PROPERTY_MANAGEMENT_SERVICES}
       />
     </div>
