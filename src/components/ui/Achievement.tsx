66034a1189a3db5d7eb16df387efa03150129c63
Abdalrhman Almarakeby <aalmarakeby@gmail.com>
2024-05-13 15:28:12 +0300

    Move Achievement component to components folder.

--- src/components/ui/Achievement.tsx
+++ src/components/ui/Achievement.tsx
@@ -10,12 +10,12 @@ export function Achievement({ achievement, label, className }: AchievementProps)
   return (
     <h2
       className={cn(
-        "last:col-span-2 space-y-0.5 border  bg-gray-10 p-4 xl:px-2  sm:!col-span-1",
+        "last:col-span-2 space-y-0.5 border bg-gray-10 p-4 xl:px-2 sm:!col-span-1",
         className
       )}
     >
       <span className="block text-2xl font-bold xl:text-3xl 3xl:text-4xl">{achievement}</span>
-      <span className="block text-primary">{label}</span>
+      <span className="text-primary block">{label}</span>
     </h2>
   );
 }
