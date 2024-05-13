6fa0ca1988307141e7a17e5068449ee267bcbaa7
Abdalrhman Almarakeby <aalmarakeby@gmail.com>
2024-05-13 20:50:20 +0300

    Update Icon container.

--- src/components/ui/IconContainer.tsx
+++ src/components/ui/IconContainer.tsx
@@ -11,7 +11,7 @@ export function IconContainer({ Icon, className }: IconContainerProps) {
   return (
     <span className={cn("relative", className)} aria-hidden="true">
       <IconContainerSVG className="size-12 animate-[spin_10s_linear] lg:size-15 3xl:size-20" />
-      <Icon className="absolute right-1/2 top-1/2 size-5 -translate-y-1/2 translate-x-1/2 fill-purple-75 lg:size-6 3xl:size-9" />
+      <Icon className=" [&>*]:!fill-purple-75 absolute right-1/2 top-1/2 size-5 -translate-y-1/2 translate-x-1/2 !fill-purple-75 lg:size-6 3xl:size-9" />
     </span>
   );
 }
