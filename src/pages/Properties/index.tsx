afe7e01a85bc700c088d4734db52f888d4137bd8
Abdalrhman Almarakeby <aalmarakeby@gmail.com>
2024-06-19 09:52:13 +0300

    Add CTA section to properties page.

--- src/pages/Properties/index.tsx
+++ src/pages/Properties/index.tsx
@@ -1,3 +1,4 @@
+import { CTA } from "@/components/ui/CTA";
 import { HeaderSection } from "@/components/ui/HeaderSection";
 
 export function Properties() {
@@ -7,6 +8,8 @@ export function Properties() {
         paragraph="Welcome to Estatein, where your dream property awaits in every corner of our beautiful world. Explore our curated selection of properties, each offering a unique story and a chance to redefine your life. With categories to suit every dreamer, your journey."
         title="Find Your Dream Property"
       />
+
+      <CTA />
     </div>
   );
 }
