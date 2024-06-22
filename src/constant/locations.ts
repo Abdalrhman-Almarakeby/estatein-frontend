6cc03b81b68883d499c317df08ceebf6f2a6c72a
Abdalrhman Almarakeby <aalmarakeby@gmail.com>
2024-06-22 12:21:51 +0300

    Update office location.

--- src/constant/locations.ts
+++ src/constant/locations.ts
@@ -3,12 +3,12 @@ const LOCATIONS = [
   {
     type: "regional",
     label: "Main Headquarters",
-    location: "123 Estatein Plaza, City Center, Metropolis",
+    location: "456 Centennial Plaza, New Troy, Metropolis",
     description:
       "Our main headquarters serve as the heart of Estatein. Located in the bustling city center, this is where our core team of experts operates, driving the excellence and innovation that define us.",
     contact: {
-      email: "info@estatein.com",
-      phone: "+1 (123) 456-7890",
+      email: "metropolis.office@estatein.com",
+      phone: "+1 416 555 7890",
       location: {
         name: "Metropolis",
         link: "https://google.com/maps",
@@ -18,18 +18,18 @@ const LOCATIONS = [
   {
     type: "international",
     label: "Regional Offices",
-    location: "456 Urban Avenue, Downtown District, Metropolis",
+    location: "456 Kensington Gardens, City of Westminster, London",
     description:
       "Estatein's presence extends to multiple regions, each with its own dynamic real estate landscape. Discover our regional offices, staffed by local experts who understand the nuances of their respective markets.",
     contact: {
-      email: "info@estatein.com",
-      phone: "+1 (123) 456-7890",
+      email: "london.office@estatein.com",
+      phone: "+44 20 7123 4567",
       location: {
-        name: "Metropolis",
+        name: "London",
         link: "https://google.com/maps",
       },
     },
   },
 ] as const;
 
-export {LOCATIONS,LOCATIONS_TYPE}
+export { LOCATIONS, LOCATIONS_TYPE };
