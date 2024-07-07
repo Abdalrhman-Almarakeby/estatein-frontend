2c10c85f9029d4cb4822e12ec69d24e1efae4594
Abdalrhman Almarakeby <almarakeby.work@gmail.com>
2024-07-07 21:00:23 +0300

    Change vite built.sourcemap to hidden for smaller file size.

--- vite.config.ts
+++ vite.config.ts
@@ -14,15 +14,15 @@ export default defineConfig({
     port: 3000,
   },
   build: {
-    sourcemap: "inline",
+    sourcemap: "hidden",
     rollupOptions: {
-      output:{
-          manualChunks(id) {
-              if (id.includes('node_modules')) {
-                  return id.toString().split('node_modules/')[1].split('/')[0].toString();
-              }
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id.toString().split("node_modules/")[1].split("/")[0].toString();
           }
-      }
-    }
+        },
+      },
+    },
   },
 });
